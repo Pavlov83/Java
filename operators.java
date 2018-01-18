23993ffd598db0ff251c96096e902e101fecb7fd
Pavel Pavlov <codedriverun@gmail.com>
2018-01-18 21:28:00 +0200

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,25 @@
+/*
+ Operator types:
+
+   Arythmetic  -,+,*,/,%,,++,--
+   Relational && || ! ^
+
+   comparators ++ !+ >< >= <=   :they are comparing and returning
+    boolean value
+    e.g.
+    a == a -> true
+    17 != -5 -> true
+    0 <= -1 -> false
+
+    Bitwise & | ^ << >> >>>   :they are used for binary representation
+    of the numbers , in terms of the logical operation they are
+    converted in binary format
+     e.g.
+     5 & 12 = ?
+      5 = 101
+      12 = 1100
+      5 & 12 = 101 & 1100 = 0100(2) = 4
+   Assigning  =, +=, -=, *=, /+, %= , &=, |=, ^=, <<=,>>=,>>>=
+   Concatenating '+'
+   Used with datatypes  e.g. (type)instanceof
+   and more...
