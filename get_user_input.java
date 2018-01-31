e06fe6c1c4475dcf4af3f910e540aecc2a3a66ea
Pavel Pavlov <codedriverun@gmail.com>
2018-01-31 22:22:10 +0200

    Add files via upload
    demonstrates the Scanner type as well as "System.in" -input stream

---
+++ get_user_input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class get_user_input{
+
+public static void main(String[] args) {
+
+  Scanner myInput = new Scanner(System.in);  //uses the system input stream
+
+  System.out.println(myInput.nextLine());   //prints what is received
+
+  }
+}
