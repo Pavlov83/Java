c4c9d8047e1e1c45d4016e44c182fd8ffdce7211
Pavlov83 <codedriverun@gmail.com>
2018-05-02 22:54:35 +0300

    added OOP Lesson one
    Lesson classes from the OOP Chapter is added

---
+++ Complete Java Course/ChapterFour/ClassesPart_1/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car audi = new Car();
+
+        porsche.model = "Carrera"; // model in class Car should be public
+
+    }
+}
