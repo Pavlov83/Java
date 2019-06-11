6cbd1fce60a59718c8df6fdcfde84f732fec5cfd
QA <qa@QAs-iMac.local>
2019-06-11 11:28:37 +0300

    characters and booleans Java

--- Complete Java Course/ChapterOne/CharAndBoolean/src/com/company/Main.java
+++ Complete Java Course/ChapterOne/CharAndBoolean/src/com/company/Main.java
@@ -4,10 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        char myChar = '\u00A9';
-        char myReg = '\u00AE';
-        System.out.println("The output character is " + myChar + myReg);
+    char myChar = '\u00A9'; //width of two bytes
+    System.out.println("Unicode output was:" + myChar);
 
+    boolean myBoolean = false;
+    boolean isTrue = true;
+
+    //Find the code for the registered symbol
+    //Create a variable of type char and assign it to the unicode value
+    //Display on screen
+
+
+        char registered = '\u00AE';
+        System.out.println(registered);
 
     }
 }
--- Complete Java Course/ChapterOne/FloatAndDouble/src/com/company/Main.java
+++ Complete Java Course/ChapterOne/FloatAndDouble/src/com/company/Main.java
@@ -1,22 +1,34 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	int myIntValue = 5;
-	float myFloatValue = 5.4F;
-	double myDoubleValue = 5d;
-        System.out.println("myIntValue = " + myIntValue);
-        System.out.println("myDoubleValue = " + myDoubleValue);
-        System.out.println("myFloatValue = " + myDoubleValue);
+
+        int myIntValue = 5 / 3; //width of 4 bytes
+        float myFloatValue = 5f / 3f; // width of 4 bytes
+        double myDoubleValue = 5d / 3d; // width of 8 bytes (faster on modern computers)
+        System.out.println("myIntValue =" + myIntValue);
+        System.out.println("myFloatValue =" + myFloatValue);
+        System.out.println("myDoubleValue =" + myDoubleValue);
 
 
+        //Convert a given number of pounds to kilograms
+        //1 create  a variable to store the number of pounds
+        //2. Calculate the number of kilograms for th number above and store it into variable
+        //3. Print the result
+        // Note: one pound is equal to 0.45359237
+
+        //We can accept the input from the console :
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter amount of pounds to convert");
+        double Pounds = scan.nextDouble();
+        double Kilograms = Pounds * 0.45359237;
+        System.out.println(Kilograms);
 
 
-    float numberOfPounds = 1;
-    float numberOfKilograms = 0.45359237F;
-    float convert = numberOfPounds * numberOfKilograms;
-    System.out.println(convert);
 
 
 
