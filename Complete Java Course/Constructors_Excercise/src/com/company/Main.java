970c1f13ee662edd0761d280e923a07c7e0e132c
Pavlov <pavka@Pavels-MacBook-Pro.local>
2019-07-08 21:19:04 +0300

    constructors

---
+++ Complete Java Course/Constructors_Excercise/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Create a class for a ban account
+        //Create fields for account number,balance,customer name,email,phone
+
+        //create geters and setters for each field
+        
+
+
+
+    }
+}
