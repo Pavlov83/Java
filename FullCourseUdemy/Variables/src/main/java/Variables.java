58b2dcabfbac60c4f3424b9d112c2b80444f1000
Pavlov83 <resolversoft@gmail.com>
2022-09-27 21:05:02 +0300

    added all datatypes

---
+++ FullCourseUdemy/Variables/src/main/java/Variables.java
@@ -0,0 +1,31 @@
+public class Variables {
+
+
+    public static void main(String[] args) {
+        //Assigning value to variable is expression
+        //Expression is construct that evaluates to single value
+
+        //Integer have 32bit width.
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        int myLastOne = myTotal - 1000;
+        System.out.println(myLastOne);
+
+        //Byte's width is 8 bits wide
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte minimum value is " + myMinByteValue);
+        System.out.println("Byte minimum value is " + myMaxByteValue);
+
+        //Short can store 16bit numbers
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        //Long can store 64bit data
+        long myLongValue = 100L;
+
+    }
+}
