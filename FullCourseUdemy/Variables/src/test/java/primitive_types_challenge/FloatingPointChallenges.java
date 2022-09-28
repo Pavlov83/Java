d100584b9d008c448bb5d66ebd1ec2d93d1b682d
Pavlov83 <resolversoft@gmail.com>
2022-09-28 23:29:39 +0300

    added float challenge

---
+++ FullCourseUdemy/Variables/src/test/java/primitive_types_challenge/FloatingPointChallenges.java
@@ -0,0 +1,21 @@
+package primitive_types_challenge;
+
+import org.testng.annotations.Test;
+
+public class FloatingPointChallenges {
+
+    public static void main(String[] args) {
+        //Create a variable with the appropriate datatype to store the number of pounds
+        //to beconverted to kilograms
+
+        float  amounOfPounds = 5;
+
+        //print  the amount of kilograms based on theamoun of punds
+
+        float amountOfKilograms = amounOfPounds  *  0.45359237F;
+        System.out.println(amountOfKilograms);
+
+    }
+
+
+}
