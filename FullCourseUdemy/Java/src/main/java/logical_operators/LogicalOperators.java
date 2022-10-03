72e314f51f0de7a83055fca672f201c8407dec86
Pavlov83 <resolversoft@gmail.com>
2022-10-03 17:15:35 +0300

    operators challenge

--- FullCourseUdemy/Java/src/main/java/logical_operators/LogicalOperators.java
+++ FullCourseUdemy/Java/src/main/java/logical_operators/LogicalOperators.java
@@ -10,17 +10,28 @@ public class LogicalOperators {
             System.out.println("Not an alien");
         }
 
-        int topScore = 100;
+        int topScore = 105;
         if(topScore >= 100){
             System.out.println("You got the top score");
         }
-
+        //Logical "AND" - evaluates to ture if both sides are true
         if(topScore>100 && !isAlien ){
             System.out.println("The top Score is 100 and not an alien");
         }
+
+        //Logical "OR" evaluates to true if one of the sides is true
+        if(topScore > 100  || !isAlien){
+            System.out.println("Either topscore is greater than 100 or is not alien");
+
+        }
     }
 
+
+
+
+
     //we can evaluate two expressions
+
 
 
 }
---
+++ FullCourseUdemy/Java/src/main/java/logical_operators/TernaryOperator.java
@@ -0,0 +1,17 @@
+package logical_operators;
+
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+        boolean isCar = true;
+
+        boolean wasCar = isCar ? true : false;
+
+        if(wasCar){
+            System.out.println("Was car is true");
+        }
+    }
+
+    //type comparison operator is "instanceOf"
+
+}
