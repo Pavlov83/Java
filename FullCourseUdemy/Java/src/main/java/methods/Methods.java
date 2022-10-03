51cb71c6bd5fb0146b8ca5370c66e679617893b1
Pavlov83 <resolversoft@gmail.com>
2022-10-03 21:50:20 +0300

    Methods

---
+++ FullCourseUdemy/Java/src/main/java/methods/Methods.java
@@ -0,0 +1,20 @@
+package methods;
+
+public class Methods {
+    public static void main(String[] args) {
+
+        calculateScore(true, 800,2,4);
+
+    }
+
+
+
+     // void method does not return value
+    public static void calculateScore(boolean gameOver,int score,int levelCompleted,int bonus){
+        gameOver = true;
+
+
+    }
+
+
+}
